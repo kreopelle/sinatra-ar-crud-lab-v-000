a406eb6a2449ef3d55791346f3809ce458435643
Kayla Reopelle <kreopelle@gmail.com>
2018-07-16 19:10:53 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
     redirect to("/posts/#{post.id}")
   end
 
-  post '/posts/:id/delete' do
+  delete '/posts/:id/delete' do
     post = Post.find(params[:id])
     post.destroy
     redirect '/posts'
